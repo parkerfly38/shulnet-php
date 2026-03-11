4abb2cef44f43ac4eac634ee2b7319391bbe77a8
Brian Kresge <brian.kresge@gmail.com>
2026-03-11 10:16:57 -0400

    GL chart of accounts capability and batch export created.

--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -46,7 +46,6 @@ export interface User {
     avatar?: string;
     email_verified_at: string | null;
     two_factor_enabled?: boolean;
-    roles?: string[];
     created_at: string;
     updated_at: string;
     [key: string]: unknown; // This allows for additional properties...
